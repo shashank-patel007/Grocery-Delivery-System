9ba0647b56c59ec05fb9380984cad899504fc1a8
Rucha Nargunde <ruchanargunde@gmail.com>
2020-11-01 00:36:22 +0530

    Final corrections to Search and Category

--- backend/online_grocery_system/products/views.py
+++ backend/online_grocery_system/products/views.py
@@ -9,13 +9,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
 import django.contrib.postgres
+import json
 
 class ProductListView(viewsets.ModelViewSet):
     permission_classes=(permissions.AllowAny,)
     queryset= Product.objects.all()
     serializer_class=  ProductSerializer
 
-    @action(detail=False,methods=['get'])
+    @action(detail=False,methods=['get','post'])
     def search(self,request):
         products=Product.objects.all()
 
@@ -25,24 +26,27 @@ class ProductListView(viewsets.ModelViewSet):
             pass
         except KeyError as e:
             pass   
-        # print(products)
-        for p in products:
-            # print(p.image)
-            p.image=r'http://127.0.0.1:8000/media/{}'.format(p.image)
-            # print(p.image)
+        
         serializer=ProductSerializer(products,many=True)
-        return Response(serializer.data,status=200)
+        # print(serializer.data)
+        serializer_dict=json.loads(json.dumps(serializer.data))
+        # print(serializer_dict)
+        for s in serializer_dict:
+            s['image']=r'http://127.0.0.1:8000{}'.format(s['image'])
+            # print(s['image'],type(s['image']))
+        return Response(serializer_dict,status=200)
     
-    @action(detail=False,methods=['get'])
+    @action(detail=False,methods=['get','post'])
     def category(self,request):
         products=Product.objects.all()
         try:
             products=products.filter(category=request.data['category'])
-            for p in products:
-                p.image=r'http://127.0.0.1:8000/media/{}'.format(p.image)
-            serializer=ProductSerializer(products,many=True)
-            return Response(serializer.data,status=200)
         except Exception as e:
             pass
         serializer=ProductSerializer(products,many=True)
-        return Response(serializer.data,status=200)
+        serializer_dict=json.loads(json.dumps(serializer.data))
+        for s in serializer_dict:
+            s['image']=r'http://127.0.0.1:8000{}'.format(s['image'])
+            # print(s['image'],type(s['image']))
+        return Response(serializer_dict,status=200)
+       
